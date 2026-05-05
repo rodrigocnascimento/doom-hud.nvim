90f96e234ade109195c286163b64d2e1e1ddab5c
rodrigo nascimento <rodregoc@gmail.com>
2026-05-05 20:14:53 -0300

    feat: fix init

--- lua/doom_hud/init.lua
+++ lua/doom_hud/init.lua
@@ -1,18 +1,10 @@
 local M = {}
 
 function M.setup(opts)
-  opts = opts or {}
-  
-  vim.g.colors_name = "doom"
-  -- 1. Carrega o motor de destaques
-  require("doom_hud.theme").apply()
-  
-  -- Se o Lualine estiver instalado no ambiente, injeta o HUD automaticamente
-  local status_ok, lualine = pcall(require, "lualine")
-  if status_ok then
-    local config = require("doom_hud.lualine").get_config()
-    lualine.setup(config)
-  end
+	opts = opts or {}
+	-- Aplica apenas o motor de cores e bordas
+	require("doom_hud.theme").apply()
+	vim.g.colors_name = "doom"
 end
 
 return M
