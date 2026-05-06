027c1bab0964a26f290cb0d5d08ac84b53623c8b
rodrigo nascimento <rodregoc@gmail.com>
2026-05-06 00:42:04 -0300

    feat: restaurar integracao lualine automatica
    Plugin agora se configura automaticamente com todos os indicadores HUD.

--- lua/doom_hud/init.lua
+++ lua/doom_hud/init.lua
@@ -1,9 +1,17 @@
 local M = {}
-local colors = require("doom_hud.palette")
 
 function M.setup(opts)
 	opts = opts or {}
 	require("doom_hud.theme").apply()
+
+	if opts.integrate_lualine ~= false then
+		vim.defer_fn(function()
+			local lualine_ok, lualine = pcall(require, "lualine")
+			if lualine_ok then
+				lualine.setup(require("doom_hud.lualine").get_config())
+			end
+		end, 100)
+	end
 end
 
 return M
