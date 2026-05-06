3e373976288792578bde8d762cf3a91ccd8d9eb5
rodrigo nascimento <rodregoc@gmail.com>
2026-05-05 21:34:51 -0300

    fix(init): usar UIEnter com defer_fn para garantir ordem de setup
    Garantir que a configuração da lualine rode por último após todos os outros plugins.

--- lua/doom_hud/init.lua
+++ lua/doom_hud/init.lua
@@ -8,14 +8,19 @@ function M.setup(opts)
 
 	-- Integra lualine automaticamente se disponível e habilitado
 	if opts.integrate_lualine ~= false then
-		-- Usar VimEnter para garantir que lualine já foi carregado
-		vim.api.nvim_create_autocmd("VimEnter", {
+		-- Função para configurar lualine
+		local function setup_lualine()
+			local lualine_ok, lualine = pcall(require, "lualine")
+			if lualine_ok then
+				lualine.setup(require("doom_hud.lualine").get_config())
+			end
+		end
+
+		-- Aguardar UIEnter e usar defer_fn para garantir ordem de execução
+		vim.api.nvim_create_autocmd("UIEnter", {
 			once = true,
 			callback = function()
-				local lualine_ok, lualine = pcall(require, "lualine")
-				if lualine_ok then
-					lualine.setup(require("doom_hud.lualine").get_config())
-				end
+				vim.defer_fn(setup_lualine, 50)
 			end,
 		})
 	end
