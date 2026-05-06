42d1e7554a1944d937aacadd2626a7d60d6c4624
rodrigo nascimento <rodregoc@gmail.com>
2026-05-05 21:27:09 -0300

    fix(init): usar VimEnter para integracao lualine
    Garantir que lualine seja configurada apos todos os plugins estarem carregados.

--- lua/doom_hud/init.lua
+++ lua/doom_hud/init.lua
@@ -6,12 +6,18 @@ function M.setup(opts)
 	-- Aplica o colorscheme
 	require("doom_hud.theme").apply()
 
-	-- Integra lualine automaticamente se disponível
+	-- Integra lualine automaticamente se disponível e habilitado
 	if opts.integrate_lualine ~= false then
-		local lualine_ok, lualine = pcall(require, "lualine")
-		if lualine_ok then
-			lualine.setup(require("doom_hud.lualine").get_config())
-		end
+		-- Usar VimEnter para garantir que lualine já foi carregado
+		vim.api.nvim_create_autocmd("VimEnter", {
+			once = true,
+			callback = function()
+				local lualine_ok, lualine = pcall(require, "lualine")
+				if lualine_ok then
+					lualine.setup(require("doom_hud.lualine").get_config())
+				end
+			end,
+		})
 	end
 end
 
