1ea564f78627d702cb47d20acca0463a4fb36071
rodrigo nascimento <rodregoc@gmail.com>
2026-05-05 23:57:43 -0300

    feat(lualine): adicionar theme e fmt de mode

--- lua/doom_hud/lualine.lua
+++ lua/doom_hud/lualine.lua
@@ -1,14 +1,26 @@
 local M = {}
+local colors = require("doom_hud.palette")
 
 function M.get_config()
+	local theme = {
+		normal = {
+			a = { bg = colors.orange, fg = colors.bg, bold = true },
+			b = { bg = colors.bg_surface, fg = colors.fg },
+			c = { bg = colors.bg_surface, bg = colors.fg },
+		},
+	}
+
 	return {
 		options = {
+			theme = theme,
 			globalstatus = true,
 		},
 		sections = {
-			lualine_a = { "mode" },
-			lualine_b = {},
-			lualine_c = { "filename" },
+			lualine_a = {
+				{ "mode", fmt = function(str) return " DOOM: " .. str:upper() end },
+			},
+			lualine_b = { "branch", "diff" },
+			lualine_c = { { "filename", path = 1 } },
 			lualine_x = {},
 			lualine_y = {},
 			lualine_z = {},
