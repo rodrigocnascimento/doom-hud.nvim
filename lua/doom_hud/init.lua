4ade562a57a5f7e840ee895793c4d61d68ad6b00
rodrigo nascimento <rodregoc@gmail.com>
2026-05-06 01:05:05 -0300

    fix(init): remover integracao automatica lualine - usar opts no LazyVim

--- lua/doom_hud/init.lua
+++ lua/doom_hud/init.lua
@@ -3,15 +3,6 @@ local M = {}
 function M.setup(opts)
 	opts = opts or {}
 	require("doom_hud.theme").apply()
-
-	if opts.integrate_lualine ~= false then
-		vim.defer_fn(function()
-			local lualine_ok, lualine = pcall(require, "lualine")
-			if lualine_ok then
-				lualine.setup(require("doom_hud.lualine").get_config())
-			end
-		end, 100)
-	end
 end
 
 return M
